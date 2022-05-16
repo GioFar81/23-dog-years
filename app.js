ef7ac0bc68aba9d7543be9b8be62e89cf5f87ad9
GioFar <farigiovanni@gmail.com>
2022-05-16 15:33:01 +0100

    Version 01, initial state.

---
+++ app.js
@@ -0,0 +1,20 @@
+//My age
+let myAge = 40;
+
+//This is the value of my first two years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//This is the ration of my later years
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+//My age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+//My name in lowercase
+myName = "Giovanni";
+
+console.log(
+    `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
